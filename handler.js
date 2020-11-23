b76a328d451771bc4a865bea19ebac34dfca008e
Paul Hutchinson <pauldevnull@gmail.com>
2020-11-23 16:27:11 -0600

    updated dependencies, cleaned up code

--- handler.js
+++ handler.js
@@ -2,78 +2,65 @@
 
 const request = require('request');
 
-/**
- * Use this command to launch the handler from console:
- *
- * node_modules/.bin/serverless invoke local -f lambda -d '{"httpMethod":"GET","queryStringParameters":{"url":"http://github.com"}}'
- *
- *  or from browser
- *
- * http://localhost:3000/?url=https://github.com
- */
 module.exports.corsProxy = (event, context, callback) => {
-    let params = event.queryStringParameters;
-    let { Host, host, Origin, origin, ...headers } = event.headers;
+  const params = event.queryStringParameters;
+  const { Host, host, Origin, origin, ...headers } = event.headers;
 
-    console.log(event);
-    console.log(`Got request with params:`, params);
+  console.log(event);
+  console.log(`Got request with params:`, params);
 
-    if (!params.url) {
-        const errorResponse = {
-            statusCode: 400,
-            body: 'Unable get url from \'url\' query parameter'
-        };
+  if (!params.url) {
+    const errorResponse = {
+      statusCode: 400,
+      body: 'Unable get url from \'url\' query parameter'
+    };
 
-        callback(null, errorResponse);
+    callback(null, errorResponse);
 
-        return;
+    return;
+  }
+
+  return new Promise((resolve, reject) => {
+    const originalRequestBody = event.body;
+    let requestUrl = params.url;
+
+    for (const param of Object.keys(params)) {
+      if (param !== 'url') {
+        requestUrl += requestUrl.includes('?') ? ('&' + param + '=' + params[param]) : (':' + param + '=' + params[param]);
+      }
     }
 
-    return new Promise((resolve, reject) => {
-        let originalRequestBody = event.body;
-        let requestUrl = params.url;
+    request({
+      url: requestUrl,
+      method: event.httpMethod,
+      timeout: 20000,
+      json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
+      headers: { ...headers, 'Accept-Encoding': '*' },
+    }, (err, originalResponse, body) => {
+      if (err) {
+        console.log(`Got error`, err);
+        callback(err);
+        reject(err);
+        return;
+      }
 
-        // TODO: clean this up
-        for (const param of Object.keys(params)) {
-            if (param !== 'url') {
-                requestUrl += requestUrl.includes('?') ? ('&' + param + '=' + params[param]) : (':' + param + '=' + params[param]);
-            }
-        }
+      console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
+      const proxyBody = originalRequestBody ? JSON.stringify(body) : originalResponse.body;
 
-        request({
-            url: requestUrl,
-            method: event.httpMethod,
-            timeout: 20000,
-            json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
-            headers: { ...headers, 'Content-Type': 'application/json', 'Accept-Encoding': '*' },
-        }, (err, originalResponse, body) => {
-            if (err) {
-                console.log(`Got error`, err);
+      const proxyResponse = {
+        statusCode: originalResponse.statusCode,
+        headers: {
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Credentials' : true,
+          'Content-Type': originalResponse.headers['content-type'],
+        },
+        body: proxyBody
+      };
 
-                callback(err);
+      callback(null, proxyResponse);
 
-                reject(err);
-
-                return;
-            }
-
-            console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
-            const proxyBody = originalRequestBody ? JSON.stringify(body) : originalResponse.body;
-
-            const proxyResponse = {
-                statusCode: originalResponse.statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    'Access-Control-Allow-Headers': '*',
-                    "content-type": originalResponse.headers['content-type']
-                },
-                body: proxyBody
-            };
-
-            callback(null, proxyResponse);
-
-            resolve(proxyResponse);
-        });
+      resolve(proxyResponse);
     });
+  });
 };
